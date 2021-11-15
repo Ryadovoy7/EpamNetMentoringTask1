73420f4ca96043d6ec5701f0d907f336f7b6be3d
Ryadovoy 7 <ryadovoy7@gmail.com>
2021-11-16 02:48:45 +0400

    Добавьте файлы проекта.

---
+++ ClassLibrary/UtilityHelloWorld.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class UtilityHelloWorld
+    {
+        public static string HelloWorld (string name) => $"{System.DateTime.Now} Hello, {name}!";
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ClassLibrary.UtilityHelloWorld.HelloWorld(args.Length > 0 ? args[0] : null));
+        }
+    }
+}
